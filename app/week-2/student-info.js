d5105e45df6c3c6f6680119751e300260ad855e5
jcheema2004 <137137788+jcheema2004@users.noreply.github.com>
2025-09-09 14:26:08 -0600

    completed week 2 assignment
    added and updated student-info in assignment and added link to repository.

--- app/week-2/student-info.js
+++ app/week-2/student-info.js
@@ -1,1 +1,13 @@
+import Link from "next/link";
 
+export default function StudentInfo(){
+
+
+    return (
+        <div>
+            <h3>Jasmine Cheema</h3>
+            <Link href="https://github.com/jcheema2004/cprg306-assignments?tab=readme-ov-file">Github link to repository</Link>
+            
+        </div>
+    )
+}
